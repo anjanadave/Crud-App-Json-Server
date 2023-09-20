c1caa7a6288509e254936bf69362f5bfe3c85b66
anjanadave <anju.aksh72@gmail.com>
2023-09-20 15:36:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import AllUsers from './component/AllUsers';
+import AddUser from './component/AddUser';
+import EditUser from './component/EditUser';
+import NavBar from './component/NavBar';
+import NotFound from './component/NotFound';
+import Home from './component/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/all" element={<AllUsers />} />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/component/NotFound.js
@@ -0,0 +1,7 @@
+
+const NotFound = () => {
+return (
+<img src='https://cdn.pixabay.com/photo/2016/04/24/22/30/monitor-1350918_640.png' style={{width: '30%', margin: '80px 0 0 35%'}}/>
+)
+}
+export default NotFound;
